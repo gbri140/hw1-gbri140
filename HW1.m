73ad5de63aeed2132fd12e3f5e8fca4b4027d8b0
gbri140 <glb3@rice.edu>
2016-09-06 12:41:02 -0500

    Answers

--- HW1.m
+++ HW1.m
@@ -9,8 +9,16 @@
 %your code should work no matter which of these lines is uncommented. 
 x = 3; y = 5; % integers
 %x = '3'; y= '5';
-
+%z = x + y;
 %your code goes here
+
+if ischar (x)
+    x1 = str2num(x);
+    y1 = str2num(y);
+    z2=x1+y1;
+else 
+    z=x+y;
+end     
 
 %output your answer
 
@@ -23,14 +31,21 @@ x = 3; y = 5; % integers
 % you can do this without using a loop.
 
 N = 500; % define sequence length
-
-
+ATGCdna = ['A', 'T', 'G', 'C'];
+rand_seq = ATGCdna(randi(4, 1, N));
 
 %part 2: open reading frames (ORFs) are pieces of DNA that can be
 % transcribed and translated. they start with a start codon (ATG) and end with a
 % stop codon (TAA, TGA, or TAG). Write a piece of code that finds the longest ORF 
 % in your seqeunce rand_seq. Hint: see the function strfind.
-
+ATG = strfind(rand_seq, 'ATG');
+stop_codon_positions = [strfind(rand_seq, 'TGA') strfind(rand_seq, 'TAG') strfind(rand_seq, 'TAA')]; 
+for i = 1: length (ATG)
+   for j =1: length (stop_codon_positions);
+  %orf = stop_codon_positions(i) - ATG(i);
+  
+   end;
+end;    
 %part 3: copy your code in parts 1 and 2 but place it inside a loop that
 % runs 10000 times. Use this repeating operation to determine the probability
 % that an sequence of length 500 has an ORF of greater than 50 b.p.
@@ -57,11 +72,17 @@ N = 500; % define sequence length
 
 % part1: write code to read the Cp data from this file into a vector. You can ignore the last two
 % rows with positions beginning with G and H as there were no samples here. 
+filename='qPCRdata.txt';
+qPCRfile=importdata(filename);
+qPCRfile.data(1:72,1);
 
 % Part 2: transform this vector into an array representing the layout of
 % the plate. e.g. a 6 row, 12 column array should that data(1,1) = Cp from
 % A1, data(1,2) = Cp from A2, data(2,1) = Cp from B1 etc. 
 
+for i=1:6
+    originalplate(i,:)=[qPCRfile.data(i*12-11:i*12)'];
+end 
 % Part 3. The 4th gene in columns 10 - 12 is known as a normalization gene.
 % That is, it's should not change between conditions and it is used to normalize 
 % the expression values for the others. For the other three
@@ -71,6 +92,23 @@ N = 500; % define sequence length
 % the gene in the 1st condition, CpX is the value of Cp in condition and
 % CpN0 and CpNX are the same quantitites for the normalization gene.
 % Plot this data in an appropriate way. 
+for i=1:6
+    for j=1:4
+        average(i,j)= mean(originalplate(i,(j*3)-2:(j*3-2)+2));    
+    end 
+end
+for i=1:5
+   for  j=1:3   
+   normal(i,j)= [2^(average(1,j) - average(i+1,j) - (average(1,4) - average(i+1,4)))];    
+end 
+end
+x=2:6;
+plot (x,normal)
+legend('id1', 'pai1', 'smad7')
+title('qPCR plot')
+ylabel('fold change')
+xlabel('condtions')
+
 
 %% problem 4. starting with image data:
 
@@ -83,9 +121,22 @@ N = 500; % define sequence length
 % project (that is, each pixel in the image should contain the maximal
 % value for that pixel in the entire z-stack. store this in the variable
 % max_img
+Files = dir('/Users/George/Desktop/Classes/QuantBio/Hw1/flyData/*.tif');
+for i=1:384
+    filename=strcat('/Users/George/Desktop/Classes/QuantBio/Hw1/flyData/',Files(i).name);
+    images{i}=imread(filename);
+    
+end
+max_img=max(cat(3,images{:}),[],3);
 
 % part 2. write a loop that reads all the images and writes them as a
 % single 3D image. hint: see the function imwrite and its option 'append' 
 %
-
+Files=dir('/Users/George/Desktop/Classes/QuantBio/Hw1/flyData/*.tif');
+n=length(Files);
+for ii=1:n
+    image=imread(['z',num2str(ii),'.tif']);
+    imwrite(image,'fileimage.tif','WriteMode','append','Compression','none')
+    
+end
 
